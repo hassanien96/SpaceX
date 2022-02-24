a714c8ef2b5f11fe4ba411702b3aa8e62f8e8ec7
hassanein <Hassanainxxx@gmail.com>
2022-02-24 20:49:50 +0300

    first commit

--- app/src/main/java/com/example/spacexassignment/model/network/responses/launches/Rocket.kt
+++ app/src/main/java/com/example/spacexassignment/model/network/responses/launches/Rocket.kt
@@ -6,16 +6,10 @@ import androidx.annotation.Keep
 
 @Keep
 data class Rocket(
-    @SerializedName("fairings")
-    val fairings: Fairings,
-    @SerializedName("first_stage")
-    val firstStage: FirstStage,
     @SerializedName("rocket_id")
     val rocketId: String,
     @SerializedName("rocket_name")
     val rocketName: String,
     @SerializedName("rocket_type")
     val rocketType: String,
-    @SerializedName("second_stage")
-    val secondStage: SecondStage
 )
